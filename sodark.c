957d5ab8134b86801907ba8392a37749c8f87576
Marcus Dansarie <marcus@dansarie.se>
2020-05-21 13:51:14 +0200

    Refactor sodark.c Add ability to generate random tuples with random tweaks.

--- sodark.c
+++ sodark.c
@@ -2,7 +2,7 @@
 
    Utility for encryption and decryption with the SoDark family of ciphers.
 
-   Copyright (C) 2017 Marcus Dansarie <marcus@dansarie.se>
+   Copyright (C) 2017, 2020 Marcus Dansarie <marcus@dansarie.se>
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -25,15 +25,77 @@
 
 #include "sodark.h"
 
-void printusage(char *name) {
+void printusage(const char *name) {
   fprintf(stderr, "Usage:\n");
   fprintf(stderr, "%s mode rounds pt/ct key tweak\n", name);
   fprintf(stderr, "          -3d  SoDark-3 decryption\n");
   fprintf(stderr, "          -3e  SoDark-3 encryption\n");
   fprintf(stderr, "          -6d  SoDark-6 decryption\n");
   fprintf(stderr, "          -6e  SoDark-6 encryption\n");
-  fprintf(stderr, "%s -r {3|6} rounds key tweak ntuples\n", name);
-  fprintf(stderr, "          Generate ntuples random tuples.\n");
+  fprintf(stderr, "%s -r {3|6} rounds key ntuples\n", name);
+  fprintf(stderr, "          Generate ntuples random tuples with random tweaks.\n");
+  fprintf(stderr, "%s -r {3|6} rounds key ntuples tweak\n", name);
+  fprintf(stderr, "          Generate ntuples random tuples with a specific tweak.\n");
+}
+
+int get_random(FILE *rand, uint64_t *ptr) {
+  if (fread(ptr, sizeof(uint64_t), 1, rand) != 1) {
+    fprintf(stderr, "Error when reading from /dev/urandom.\n");
+    fclose(rand);
+    return 1;
+  }
+  return 0;
+}
+
+int generate_tuples(const char *progname, const char *versionstr, const char *roundstr,
+    const char *keystr, const char *ntuplestr, const char *tweakstr) {
+  uint32_t rounds = atoi(roundstr);
+  uint64_t key    = strtoull(keystr, NULL, 16);
+  uint32_t tuples = atoi(ntuplestr);
+  uint64_t tweak = 0;
+  if (tweakstr != NULL) {
+    tweak = strtoull(tweakstr, NULL, 16);
+  }
+
+  FILE *randp = fopen("/dev/urandom", "r");
+  if (randp == NULL) {
+    fprintf(stderr, "Error when opening /dev/urandom.\n");
+    return 1;
+  }
+
+  int version = 0;
+  if (strcmp(versionstr, "3") == 0) {
+    version = 3;
+  } else if (strcmp(versionstr, "6") == 0) {
+    version = 6;
+  } else {
+    printusage(progname);
+    fclose(randp);
+    return 1;
+  }
+  fprintf(stderr, "Generating %d random tuples with SoDark-%d.\n", tuples, version);
+  fprintf(stderr, "Rounds: %d\n", rounds);
+  fprintf(stderr, "Key:    %014" PRIx64 "\n", key);
+  if (tweakstr != NULL) {
+    fprintf(stderr, "Tweak:  %016" PRIx64 "\n", tweak);
+  }
+  uint64_t pt;
+  for (int i = 0; i < tuples; i++) {
+    if (get_random(randp, &pt) || (tweakstr == NULL && get_random(randp, &tweak))) {
+      return 1;
+    }
+    if (version == 3) {
+      pt &= 0xffffff;
+      uint32_t ct = encrypt_sodark_3(rounds, (uint32_t)pt, key, tweak);
+      printf("%06x %06x %016" PRIx64 "\n", (uint32_t)pt, ct, tweak);
+    } else {
+      pt &= 0xffffffffffffL;
+      uint64_t ct = encrypt_sodark_6(rounds, pt, key, tweak);
+      printf("%012" PRIx64 " %012" PRIx64 " %016" PRIx64 "\n", pt, ct, tweak);
+    }
+  }
+  fclose(randp);
+  return 0;
 }
 
 int main(int argc, char **argv) {
@@ -52,7 +114,9 @@ int main(int argc, char **argv) {
   assert(decrypt_sodark_6(1, encrypt_sodark_6(1, 0xdeafcafebabe, 0xc2284a1ce7be2f,
       0x543bd88000017550), 0xc2284a1ce7be2f, 0x543bd88000017550) == 0xdeafcafebabe);
 
-  if (argc == 6) {
+  if (argc == 6 && strcmp(argv[1], "-r") == 0) {
+    return generate_tuples(argv[0], argv[2], argv[3], argv[4], argv[5], NULL);
+  } else if (argc == 6) {
     uint32_t rounds = atoi(argv[2]);
     uint64_t pt_ct = strtoull(argv[3], NULL, 16);
     uint64_t key   = strtoull(argv[4], NULL, 16);
@@ -65,7 +129,11 @@ int main(int argc, char **argv) {
     }
 
     printf("Rounds: %d\n", rounds);
-    printf("PT/CT:  %012" PRIx64 "\n", pt_ct);
+    if (strcmp(argv[1], "-3d") == 0 || strcmp(argv[1], "-3e") == 0) {
+      printf("PT/CT:  %06" PRIx64 "\n", pt_ct);
+    } else {
+      printf("PT/CT:  %012" PRIx64 "\n", pt_ct);
+    }
     printf("Key:    %014" PRIx64 "\n", key);
     printf("Tweak:  %016" PRIx64 "\n", tweak);
 
@@ -82,60 +150,8 @@ int main(int argc, char **argv) {
       printusage(argv[0]);
       return 1;
     }
-  } else if (argc == 7) {
-    if (strcmp(argv[1], "-r") != 0) {
-      printusage(argv[0]);
-      return 1;
-    }
-    uint32_t rounds = atoi(argv[3]);
-    uint64_t key    = strtoull(argv[4], NULL, 16);
-    uint64_t tweak  = strtoull(argv[5], NULL, 16);
-    uint32_t tuples = atoi(argv[6]);
-
-    FILE *randp = fopen("/dev/urandom", "r");
-    if (randp == NULL) {
-      fprintf(stderr, "Error when opening /dev/urandom.\n");
-      return 1;
-    }
-
-    if (strcmp(argv[2], "3") == 0) {
-      fprintf(stderr, "Generating %d random tuples with SoDark-3.\n", tuples);
-      fprintf(stderr, "Rounds: %d\n", rounds);
-      fprintf(stderr, "Key:    %014" PRIx64 "\n", key);
-      fprintf(stderr, "Tweak:  %016" PRIx64 "\n", tweak);
-      uint32_t pt;
-      for (int i = 0; i < tuples; i++) {
-        if (fread(&pt, sizeof(uint32_t), 1, randp) != 1) {
-          fprintf(stderr, "Error when reading from /dev/urandom.\n");
-          fclose(randp);
-          return 1;
-        }
-        pt &= 0xffffff;
-        uint32_t ct = encrypt_sodark_3(rounds, pt, key, tweak);
-        printf("%06x %06x %016" PRIx64 "\n", pt, ct, tweak);
-      }
-    } else if (strcmp(argv[2], "6") == 0) {
-      fprintf(stderr, "Generating %d random tuples with SoDark-6.\n", tuples);
-      fprintf(stderr, "Rounds: %d\n", rounds);
-      fprintf(stderr, "Key:    %014" PRIx64 "\n", key);
-      fprintf(stderr, "Tweak:  %016" PRIx64 "\n", tweak);
-      uint64_t pt;
-      for (int i = 0; i < tuples; i++) {
-        if (fread(&pt, sizeof(uint32_t), 1, randp) != 1) {
-          fprintf(stderr, "Error when reading from /dev/urandom.\n");
-          fclose(randp);
-          return 1;
-        }
-        pt &= 0xffffffffffffL;
-        uint64_t ct = encrypt_sodark_6(rounds, pt, key, tweak);
-        printf("%012" PRIx64 " %012" PRIx64 " %016" PRIx64 "\n", pt, ct, tweak);
-      }
-    } else {
-      printusage(argv[0]);
-      fclose(randp);
-      return 1;
-    }
-    fclose(randp);
+  } else if (argc == 7 && strcmp(argv[1], "-r") == 0) {
+    return generate_tuples(argv[0], argv[2], argv[3], argv[4], argv[5], argv[6]);
   } else {
     printusage(argv[0]);
     return 1;
